5469d9bce53f843ceb8f78b39db56832fa8aa95b
Shaoan-data <zsa5370@foxmail.com>
2019-10-25 11:21:27 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+#include<icpasz/p2picp/vanillaricp.h>
+
 int main(int argc, char **argv) {
     std::cout << "Hello, world!" << std::endl;
+    
+#ifdef FUNC_USE_SAVE_MAP
+    std::cout<<"use map"<<std::endl;
+#endif
     return 0;
 }
